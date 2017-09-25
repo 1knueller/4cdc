7fc9b821681baa1ed9740f048e29fe5313d4525f
t0b4cc0 <photosynthese@gmx.at>
2017-09-25 19:17:01 +0200

    init

---
+++ g.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+import urllib.request
+import urllib.error
+import urllib.parse
+import argparse
+import logging
+import os
+import re
+import time
+import http.client 
+import fileinput
+import time
+import itertools
+import multiprocessing
+from multiprocessing import Pool
+from multiprocessing import Process
+import lxml.html
+from bs4 import BeautifulSoup
+import django
+
+log = logging.getLogger('inb4404')
+workpath = os.path.dirname(os.path.realpath(__file__))
+args = None
+
+def load(url):
+    req = urllib.request.Request(url, headers={'User-Agent': '4chan Browser'})
+    return urllib.request.urlopen(req).read()
+
+def main():
+    fname = "linklist.txt"
+    with open(fname) as f:
+        threadlinks = f.readlines()
+    threadlinks = [line.strip() for line in threadlinks]
+    threadlinks = [line for line in threadlinks if line[:4].lower() == 'http']
+
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s', datefmt='%I:%M:%S %p')    
+
+    j = 1
+    for threadlink in threadlinks:
+        if threadlink[:4].lower() == 'http':
+            print("::: Downloading Thread " + str(j) + "/" + str(len(threadlinks)))
+            j = j + 1
+            download_thread(threadlink)
+            
+
+def process_url(directory, link_img_tuple, index):
+    link = link_img_tuple[0]
+    img = link_img_tuple[1]
+    img_path = os.path.join(directory, img)
+    if not os.path.exists(img_path):
+        start_time = time.time()
+        urllib.request.urlretrieve('https:' + link, img_path)
+        timeelapsed = time.time() - start_time
+        filesize = ((os.stat(img_path).st_size) / (1024 * 1024))
+        kbs = str(round(((filesize * 1024) / timeelapsed)))
+        print(index + " ::: " + str(round(filesize, 1)) + "MB @ " + kbs + " kilobyte/second")
+    else:
+        print(index + "already exists")
+
+def getThreadTitle(soup):
+        titles = [element.text for element in soup.find_all("span", "subject")]
+        title = max(titles, key=len)
+        threadTitleFiltered = re.sub("[^ 0-9a-zA-Z]+", "", title)[:45]
+        return threadTitleFiltered
+
+def getThumbnailLink(soup):
+        a = soup.find("a", {"class":"fileThumb"}) #get first a with class filethumb
+        img = a.find("img")                       # get tag img of a
+        return img.get("src")                   # get attribute src of tag img
+
+def mkdirs(thread_link, soup):
+        threadTitle = getThreadTitle(soup)
+        board = thread_link.split('/')[3]
+        title2 = soup.title.string.split(' - ')[1]
+        threadID = thread_link.split('/')[5].split('#')[0]
+
+        if len(threadTitle) <= 0:
+            threadTitle = (threadID + '_' + title2)[:45]
+
+        directory = os.path.join(workpath, 'downloads', board, threadTitle)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        return directory
+
+def createListForStatusDisplay(list_of_url_filename):
+        indexlist = range(1,len(list_of_url_filename) + 1)
+        list2 = list(map(str, indexlist))
+        mystring = "/" + str(len(list_of_url_filename))
+        list3 = [s + mystring for s in list2]
+        return list3
+
+def getFileLinkList(soup):
+        linklist = [link['href'] for link in soup.find_all('a',{"class":"fileThumb"}, href=True)]
+        fnames = [os.path.basename(link) for link in linklist]
+        
+        return list(zip(linklist,fnames))
+
+def download_thread(thread_link):
+    try:
+        loadedthread = load(thread_link).decode("utf-8")
+        soup = BeautifulSoup(loadedthread,"lxml")
+        directory = mkdirs(thread_link, soup)
+        
+        #urllib.request.urlretrieve("http:" + getThumbnailLink(soup), directory + "\\thumb.jpg") # stupid me didnt know windows makes thumbnails of webm anyways :/
+        while True:
+            try:
+                list_of_url_filename = list(set(re.findall('(\/\/is\d*\.4chan\.org/\w+\/(\d+\.(?:jpg|png|gif|webm)))', loadedthread)))
+                list_of_url_filename = getFileLinkList(soup)
+                pool = multiprocessing.Pool(processes=6)
+                pool.starmap(process_url, zip(itertools.repeat(directory), list_of_url_filename, createListForStatusDisplay(list_of_url_filename)))
+
+            except (urllib.error.URLError, http.client.BadStatusLine, http.client.IncompleteRead):
+                log.warning('Something went wrong')
+                time.sleep(20)
+                continue
+
+            break
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
